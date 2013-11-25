19c4d990763f7c352dcc42314296a7d437b4cc83
Toby <zhangzhuo1021@gmail.com>
2013-11-24 17:07:26 -0800

    Adding Colors.xml for default colors and create round button

--- gen/edu/berkeley/cs160/howwasyourday/R.java
+++ gen/edu/berkeley/cs160/howwasyourday/R.java
@@ -10,6 +10,11 @@ package edu.berkeley.cs160.howwasyourday;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int light_grey=0x7f040002;
+        public static final int opaque_red=0x7f040000;
+        public static final int translucent_red=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +22,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int border=0x7f020000;
@@ -43,26 +48,27 @@ public final class R {
         public static final int ic_launcher=0x7f020013;
         public static final int mom=0x7f020014;
         public static final int playing=0x7f020015;
-        public static final int tom=0x7f020016;
+        public static final int round_button=0x7f020016;
+        public static final int tom=0x7f020017;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int audio=0x7f08000f;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int draw=0x7f080010;
-        public static final int editText1=0x7f080003;
-        public static final int imageButton1=0x7f08000a;
-        public static final int imageButton2=0x7f080009;
-        public static final int imageView1=0x7f080000;
-        public static final int imageView2=0x7f080005;
-        public static final int imageView3=0x7f080007;
-        public static final int photo=0x7f08000d;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080006;
-        public static final int textView3=0x7f080008;
-        public static final int timeline=0x7f08000c;
-        public static final int video=0x7f08000e;
+        public static final int action_settings=0x7f09000b;
+        public static final int audio=0x7f09000f;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int draw=0x7f090010;
+        public static final int editText1=0x7f090003;
+        public static final int imageButton1=0x7f09000a;
+        public static final int imageButton2=0x7f090009;
+        public static final int imageView1=0x7f090000;
+        public static final int imageView2=0x7f090005;
+        public static final int imageView3=0x7f090007;
+        public static final int photo=0x7f09000d;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090006;
+        public static final int textView3=0x7f090008;
+        public static final int timeline=0x7f09000c;
+        public static final int video=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_add_comment=0x7f030000;
@@ -74,34 +80,34 @@ public final class R {
         public static final int activity_timeline=0x7f030006;
     }
     public static final class menu {
-        public static final int add_comment=0x7f070000;
-        public static final int draw_doodle=0x7f070001;
-        public static final int post=0x7f070002;
-        public static final int record_audio=0x7f070003;
-        public static final int record_video=0x7f070004;
-        public static final int share_photo=0x7f070005;
-        public static final int timeline=0x7f070006;
+        public static final int add_comment=0x7f080000;
+        public static final int draw_doodle=0x7f080001;
+        public static final int post=0x7f080002;
+        public static final int record_audio=0x7f080003;
+        public static final int record_video=0x7f080004;
+        public static final int share_photo=0x7f080005;
+        public static final int timeline=0x7f080006;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int add_comment=0x7f050009;
-        public static final int app_name=0x7f050000;
-        public static final int audio=0x7f050004;
-        public static final int draw=0x7f050005;
-        public static final int hello_world=0x7f050008;
-        public static final int photo=0x7f050002;
-        public static final int timeline=0x7f050006;
-        public static final int title_activity_add_comment=0x7f05000a;
-        public static final int title_activity_doodle=0x7f05000b;
-        public static final int title_activity_draw_doodle=0x7f05000c;
-        public static final int title_activity_post=0x7f050007;
-        public static final int title_activity_record_audio=0x7f05000f;
-        public static final int title_activity_record_video=0x7f05000e;
-        public static final int title_activity_share_photo=0x7f05000d;
-        public static final int video=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int add_comment=0x7f060009;
+        public static final int app_name=0x7f060000;
+        public static final int audio=0x7f060004;
+        public static final int draw=0x7f060005;
+        public static final int hello_world=0x7f060008;
+        public static final int photo=0x7f060002;
+        public static final int timeline=0x7f060006;
+        public static final int title_activity_add_comment=0x7f06000a;
+        public static final int title_activity_doodle=0x7f06000b;
+        public static final int title_activity_draw_doodle=0x7f06000c;
+        public static final int title_activity_post=0x7f060007;
+        public static final int title_activity_record_audio=0x7f06000f;
+        public static final int title_activity_record_video=0x7f06000e;
+        public static final int title_activity_share_photo=0x7f06000d;
+        public static final int video=0x7f060003;
     }
     public static final class style {
-        public static final int ActionBarStyle=0x7f060002;
+        public static final int ActionBarStyle=0x7f070002;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -123,9 +129,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
