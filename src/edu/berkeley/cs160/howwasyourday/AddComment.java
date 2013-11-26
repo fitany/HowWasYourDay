b704c6ce16bcf97bca90c2d7e2fc849c8945fcb9
Tiffany Hwu <fitany@gmail.com>
2013-11-26 02:01:35 -0800

    undo accidental comment out of marks code

--- src/edu/berkeley/cs160/howwasyourday/AddComment.java
+++ src/edu/berkeley/cs160/howwasyourday/AddComment.java
@@ -26,8 +26,6 @@ public class AddComment extends Activity {
                 null);
         actionBar.setCustomView(view);
         
-
-       /* 
         final Button buttonBright = (Button) findViewById(R.id.imageButton5);
         final Button buttonDark = (Button) findViewById (R.id.imageButton6);
         final Button buttonRotateClockwise = (Button) findViewById (R.id.imageButton4);
@@ -132,7 +130,7 @@ public class AddComment extends Activity {
             	}
         });
         
-	*/}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
