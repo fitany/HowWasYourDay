7f6f13ef8894533f3114d67ced03aa7b9b18405a
Jane Luo <jane.luo8l@gmail.com>
2013-11-25 23:04:53 -0800

    colors work

--- src/edu/berkeley/cs160/howwasyourday/DrawDoodle.java
+++ src/edu/berkeley/cs160/howwasyourday/DrawDoodle.java
@@ -3,11 +3,13 @@ package edu.berkeley.cs160.howwasyourday;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.Toast;
 
 public class DrawDoodle extends Activity {
 	
@@ -48,6 +50,39 @@ public class DrawDoodle extends Activity {
 	
 	public void clear(View v) {
 		drawArea.clear();
+	}
+	public void getColor(View v) {
+		int loc = 0;
+		if(v.getId()== R.id.blue_button) {
+			Toast.makeText(this, "You have chosen blue.",
+			Toast.LENGTH_SHORT).show();
+			loc = Color.BLUE; 
+		} else if (v.getId()== R.id.red_button) {
+			Toast.makeText(this, "You have chosen red.",
+					Toast.LENGTH_SHORT).show();
+					loc = Color.RED; 
+		} else if (v.getId()== R.id.green_button) {
+			Toast.makeText(this, "You have chosen green.",
+					Toast.LENGTH_SHORT).show();
+					loc = Color.GREEN; 
+		} else if (v.getId()== R.id.yellow_button) {
+			Toast.makeText(this, "You have chosen yellow.",
+					Toast.LENGTH_SHORT).show();
+					loc = Color.YELLOW; 
+			
+		} else if (v.getId()== R.id.purple_button) {
+			Toast.makeText(this, "You have chosen purple.",
+					Toast.LENGTH_SHORT).show();
+					loc = Color.parseColor("purple"); 
+		} else if (v.getId()== R.id.Eraser) {
+			Toast.makeText(this, "You have chosen eraser.",
+					Toast.LENGTH_SHORT).show();
+					loc = Color.WHITE; 
+		} else {
+		
+		}
+		
+		drawArea.setColor(loc);
 	}
 	
 	public void done(View v){
