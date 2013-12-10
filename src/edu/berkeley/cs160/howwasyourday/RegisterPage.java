3b23066d6a64deed883d63ee22842d333835d5e7
Toby <zhangzhuo1021@gmail.com>
2013-12-10 01:33:56 -0800

    Finally make resgiterpage fully functional

--- src/edu/berkeley/cs160/howwasyourday/RegisterPage.java
+++ src/edu/berkeley/cs160/howwasyourday/RegisterPage.java
@@ -7,15 +7,22 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import edu.berkeley.cs160.howwasyourday.database.DatabaseHelper;
 
-public class RegisterPage extends Activity {
+public class RegisterPage extends Activity implements TextWatcher {
 	
 	private EditText email;
 	private EditText password;
@@ -25,6 +32,13 @@ public class RegisterPage extends Activity {
 	private DatabaseHelper db;
 	private SQLiteDatabase database;
 	private ArrayAdapter adapter;
+	private Button register;
+	AutoCompleteTextView autoComplete;
+	Family[] families;
+	String[] familyNames;
+	TextView textViewSelection;
+	TextView createNewFamily;
+	EditText name;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,14 @@ public class RegisterPage extends Activity {
 		lastName = (EditText) findViewById(R.id.lastname);
 		firstName = (EditText) findViewById(R.id.firstname);
 		userType= (Spinner) findViewById(R.id.userType);
+		register = (Button) findViewById(R.id.register);
+		createNewFamily = (TextView) findViewById(R.id.newFamily);
+		createNewFamily.setVisibility(View.INVISIBLE);
+		
+		autoComplete = (AutoCompleteTextView) findViewById(R.id.family);
+        autoComplete.addTextChangedListener(this);
+		
+		updateFamilies();
 		
 		Bundle b = getIntent().getExtras();
 		String emailText = b.getString("emailText");
@@ -53,6 +75,111 @@ public class RegisterPage extends Activity {
 		userType.setVisibility(View.VISIBLE);
 		userType.setPrompt("Select User Type");
 		
+		userType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View v, int position, long id) {
+				// TODO Auto-generated method stub
+				if (position == 1) {
+					createNewFamily.setVisibility(View.VISIBLE);
+				} else {
+					createNewFamily.setVisibility(View.INVISIBLE);
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+			}
+        	
+        });
+		
+		register.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String emailText = email.getText().toString();
+				String passwordText = password.getText().toString();
+				String lastNameText = lastName.getText().toString();
+				String firstNameText = firstName.getText().toString();
+				String userTypeText = userType.getSelectedItem().toString();
+				String familyNameText = autoComplete.getText().toString();
+				if (emailText.equals("")) {
+					popupWindow("Email can't be empty!");
+				} else if (firstNameText.equals("")) {
+					popupWindow("First Name can't be empty!");
+				} else {
+					int pos = findFamily(familyNameText);
+					if (userTypeText.equals("Children")) {
+						if (pos >= 0) {
+							db.createUser(database, emailText, passwordText,firstNameText, lastNameText, userTypeText, families[pos].id);
+							Cursor curUser = db.findUser(database, emailText);
+							curUser.moveToFirst();
+							User user = new User(curUser.getInt(curUser.getColumnIndex("UserId")), curUser.getString(curUser.getColumnIndex("UserFirstName")), curUser.getString(curUser.getColumnIndex("UserLastName")), curUser.getString(curUser.getColumnIndex("UserType")));
+							LoginPage.setCurUser(user);
+							done();
+							
+						} else {
+							popupWindow("No Family Record Found!");
+						}
+					} else {
+						if (pos >= 0) {
+							db.createUser(database, emailText, passwordText,firstNameText, lastNameText, userTypeText, families[pos].id);
+							Cursor curUser = db.findUser(database, emailText);
+							curUser.moveToFirst();
+							User user = new User(curUser.getInt(curUser.getColumnIndex("UserId")), curUser.getString(curUser.getColumnIndex("UserFirstName")), curUser.getString(curUser.getColumnIndex("UserLastName")), curUser.getString(curUser.getColumnIndex("UserType")));
+							LoginPage.setCurUser(user);
+							done();
+						} else {
+							popupWindow("No Family Record Found! Please Register First");
+						}
+					}
+				}
+			}
+		});
+		
+		createNewFamily.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				register();
+			}
+			
+		});
+		
+	}
+	
+	private void updateFamilies() {
+		families = db.getAllFamilies(database);
+		familyNames = new String[families.length];
+		int index = 0;
+		for (Family family: families) {
+			familyNames[index] = family.name;
+			index++;
+		}
+		
+		autoComplete.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, familyNames));
+	}
+	
+	@Override
+	public void afterTextChanged(Editable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -62,26 +189,19 @@ public class RegisterPage extends Activity {
 		return true;
 	}
 	
-	
-	public void done(View v) {
-		String emailText = email.getText().toString();
-		String passwordText = password.getText().toString();
-		String lastNameText = lastName.getText().toString();
-		String firstNameText = firstName.getText().toString();
-		String userTypeText = userType.getSelectedItem().toString();
-		if (emailText.equals("")) {
-			popupWindow("Email can't be empty!");
-		} else if (firstNameText.equals("")) {
-			popupWindow("First Name can't be empty!");
-		} else {
-			db.createUser(database, emailText, passwordText,firstNameText, lastNameText, userTypeText);
-			Cursor curUser = db.findUser(database, emailText);
-			curUser.moveToFirst();
-			User user = new User(curUser.getInt(curUser.getColumnIndex("UserId")), curUser.getString(curUser.getColumnIndex("UserFirstName")), curUser.getString(curUser.getColumnIndex("UserLastName")), curUser.getString(curUser.getColumnIndex("UserType")));
-			LoginPage.setCurUser(user);
-			Intent i = new Intent(this, Timeline.class);
-			startActivity(i);
+	public int findFamily(String name) {
+		for (int i =0; i < familyNames.length; i++) {
+			if (name.equals(familyNames[i])) {
+				return i;
+			}
 		}
+		
+		return -1;
+	}
+	
+	public void done() {
+		Intent i = new Intent(this, Timeline.class);
+		startActivity(i);
 	}
 	
 	public void goBack(View v) {
@@ -103,6 +223,39 @@ public class RegisterPage extends Activity {
 				dialog.cancel();
 			}
 		});
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+		
+	}
+	
+	private void register() {
+		
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		LayoutInflater inflater = this.getLayoutInflater();
+		View layout=inflater.inflate(R.layout.new_family,null);
+		alertDialogBuilder.setView(layout);
+		
+		name =(EditText) layout.findViewById(R.id.familyname);
+		
+		alertDialogBuilder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	               // User clicked OK button
+	        	   db.createFamily(database, name.getText().toString());
+	        	   autoComplete.setText(name.getText().toString());
+	        	   updateFamilies();
+	        	   dialog.dismiss();
+	           }
+	       });
+		alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	               // User cancelled the dialog
+	        	   dialog.cancel();
+	           }
+	       });
 
 		// create alert dialog
 		AlertDialog alertDialog = alertDialogBuilder.create();
--- src/edu/berkeley/cs160/howwasyourday/database/DatabaseHelper.java
+++ src/edu/berkeley/cs160/howwasyourday/database/DatabaseHelper.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import edu.berkeley.cs160.howwasyourday.Family;
 import edu.berkeley.cs160.howwasyourday.PostEntry;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -40,14 +41,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	static final String PostTime="PostTime";
 	
 	public DatabaseHelper(Context context) {
-		super(context, dbName, null, 8); 
+		super(context, dbName, null, 13); 
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		  // TODO Auto-generated method stub
 		  
-		  db.execSQL("CREATE TABLE "+familyTable+" ("+FamilyId+ " INTEGER PRIMARY KEY , "+FamilyName+ " TEXT UNIQUE)");
+		  db.execSQL("CREATE TABLE "+familyTable+" ("+FamilyId+ " INTEGER PRIMARY KEY AUTOINCREMENT, "+FamilyName+ " TEXT UNIQUE)");
 		  
 		  db.execSQL("CREATE TABLE "+userTable+" ("+UserId+" INTEGER PRIMARY KEY AUTOINCREMENT, "+ UserEmail +" TEXT NOT NULL UNIQUE, "+ UserPassword +" TEXT NOT NULL, " + UserFirstName +" TEXT, " + UserLastName +" TEXT, " +UserType+ " TEXT, " + UserFamilyId + " INTEGER ,FOREIGN KEY ("+UserFamilyId+") REFERENCES "+familyTable+" ("+FamilyId+"));");
 		  
@@ -65,14 +66,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	    onCreate(db);
 	}
 	
-	public void createUser(SQLiteDatabase db, String email, String password, String firstname, String lastname, String userType) {
+	public void createFamily(SQLiteDatabase db, String name) {
+		ContentValues cv=new ContentValues();
+	    cv.put(FamilyName, name);
+	    db.insert(familyTable, null, cv);
+	}
+	
+	public void createUser(SQLiteDatabase db, String email, String password, String firstname, String lastname, String userType, long familyId) {
 		ContentValues cv=new ContentValues();
 	    cv.put(UserEmail, email);
 	    cv.put(UserPassword, password);
 	    cv.put(UserFirstName, firstname);
 	    cv.put(UserLastName, lastname);
 	    cv.put(UserType, userType);
-	    cv.put(UserFamilyId, 1);
+	    cv.put(UserFamilyId, familyId);
 	    db.insert(userTable, null, cv);
 	}
 	
@@ -80,6 +87,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		Cursor c = db.query(userTable, new String[] {UserId, UserPassword, UserFirstName, UserLastName,UserType}, UserEmail+"=?", new String[]{email}, null,null, null);
 	    return c;
 	}
+	
 	
 	public void savePic(SQLiteDatabase db, long userID, String discription, int feeling, String path) {
 		ContentValues cv=new ContentValues();
@@ -116,11 +124,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public String[] findFamily(SQLiteDatabase db,long id) {
 		Cursor c = db.query(userTable, new String[] {UserId}, UserFamilyId+"=?", new String[] {id+""}, null,null, null);
 		String[] result = new String[c.getCount()];
-		System.out.println(c.getCount());
 		int index = 0;
 		while(c.moveToNext()) {
 			result[index] = c.getInt(0)+"";
-			System.out.println(index);
 			index++;
 		}
 		
@@ -162,6 +168,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	    
 	    return posts;
 	}
+	
+	public Family[] getAllFamilies(SQLiteDatabase db) {
+		Cursor c = db.rawQuery("select * from Families", null);
+		Family[] result = new Family[c.getCount()];
+    	
+		int index = 0;
+		while(c.moveToNext()) {
+			result[index] = new Family(c.getInt(c.getColumnIndex("FamilyId")), c.getString(c.getColumnIndex("FamilyName")));
+			index++;
+		}
+		
+		return result;
+    }
 	
 }
 	
